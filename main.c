44835e579ee3259eb25eed3e3cee9c80f0975374
Ni7r4m <georgediomartin@gmail.com>
2022-05-08 17:54:17 +0100

    Fixed and added formatting to output

--- main.c
+++ main.c
@@ -4,21 +4,28 @@
 
 //insert time logic command here
 
-struct schedule {
-    char *name;
-    char *duration; 
+struct schedules {
+    char *name[30];
+    char *duration[4]; 
     int priority;
 };
 
 int main()
 {
-    struct schedule one;
+    struct schedules schedule1;
     printf("Enter the name of the event... (30 characters max)\n");
-    scanf ("%s", one.name);
+    scanf ("%s", schedule1.name);
     printf("Enter the priority of the event... (1 for earliest)\n");
-    scanf ("%d", &one.priority);
+    scanf ("%d", &schedule1.priority);
     printf("Enter the duration of the event... (format HHMM)\n");
-    scanf ("%s", one.duration);
+    scanf ("%s", schedule1.duration);
 
-    printf("You have entered that the event %s is on the %d priority and has a duration of %s.\n", one.name, one.priority, one.duration);
+
+
+printf("\n");
+printf("%-35s %-13s %s", "Name", "Priority", "Duration\n");
+printf("%-35s %-13s %s", "----------------------------------", "------------", "----------\n");
+printf("%-35s %-13d %s", schedule1.name, schedule1.priority, schedule1.duration);
+
+return 0;
 }
