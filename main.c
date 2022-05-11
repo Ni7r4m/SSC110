c6f53605a5df3f9f081bb5836d92b9a2b47f183b
Ni7r4m <georgediomartin@gmail.com>
2022-05-11 14:52:17 +0100

    Added functionality in some syntaxes

--- main.c
+++ main.c
@@ -3,38 +3,77 @@
 #include <stdlib.h>
 
 
-//insert time logic command here
+//insert time logic command here for calculation (should return day, time)
+
+
 
 struct schedules {
-    char *name[30];
-    char *duration[4]; 
+    char name[30];
+    char duration[4]; 
     int priority;
 };
 
 int main()
 {
-    //char *input[10];
+    char input[10];
+    char tmpname[30];
+    int tmppriority;
+    char tmpduration[4];
+    int i;
+    int numUsed;
+
+struct schedules schedule[100];
+
     printf("\n" "Starting Festival Scheduler... \n" "\n" "Type 'help' to show lists of syntaxes in the app.\n");
 
-    /*
+    
     while (1)
     {
-        printf("\n" "SCHEDULER>")
+        printf("\n" "SCHEDULER>");
         scanf ("%s", input);
         
-        if(insert strcmp help command here){  //if user types the help command.
-            printf("\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n",
+        if(strcmp(input, "help") == 0){  //if user types the help command.
+            printf("\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n"  "%-10s %s\n" "%-10s %s\n" "%-10s %s\n",
             "ADD", "Adds an event from the schedule.",
             "DEL", "Removes an event from the schedule.",
-            "NEXT", "Shows the next day of the festival (up to day 7).",
-            "PREV", "Shows the previous day of the festival",
+            "EXIT", "Quits Festival Scheduler.", 
+            "EXPORT", "Exports the events into a txt file.",
             "IMPORT", "Imports events from a txt file.",
-            "EXIT", "Quits Festival Scheduler."); 
-        };
+            "LIST", "Shows all the list of events in Festival Scheduler."); 
+        }
 
-        if (strcmp del) { //if user types the del command.
+        else if(strcmp(input, "add") == 0){
+
+            printf("Enter the name of the event... (30 characters max)\n");
+                scanf ("%s", schedule[0].name);
+                if (strlen(tmpname)<30){
+                    printf("Enter the priority of the event... (leave it blank to put the event below an existing one)\n");
+                    scanf ("%d", &schedule[0].priority);
+                    printf("Enter the duration of the event... (format HHMM)\n"); 
+                    scanf ("%s", schedule[0].duration);
+
+                /*printf("Enter the name of the event... (30 characters max)\n");
+                scanf ("%s", &tmpname);
+                if (strlen(tmpname)<30){
+                    printf("Enter the priority of the event... (leave it blank to put the event below an existing one)\n");
+                    scanf ("%d", &tmppriority);
+                    printf("Enter the duration of the event... (format HHMM)\n"); 
+                    scanf ("%s", &tmpduration);
+
+                   /* i = tmppriority - 1;
+                    schedule[0].name = tmpname;
+                    schedule[0].priority = tmppriority;
+                    schedule[0].duration = tmpduration; */
+
+                    printf("Added event successfully.\n");
+                }
+                else {
+                    printf("Error! Event name is over 30 characters long.\n");
+                }
+        }
+        /*else if (input, "del") == 0) { //if user types the del command.
             printf("\n" "Please select an event to delete based on the priority number. \n");
-            scanf ("%s", input);
+            scanf ("%d", &tmppriority);
 
             if(priority number is found on the schedule); {
                 printf("You are going to remove the selected event:\n" 
@@ -44,6 +83,8 @@ int main()
 
                 if (yes); {
                     //insert remove command here
+                    //updates all the priority
+                    printf("Deleted event successfully.\n")
                 }
             }
             else() {
@@ -56,36 +97,34 @@ int main()
             scanf ("%s", input);
 
             if(success) {
-                printf("\n" "Import successful!");
+                printf("\n" "Import successful!\n");
             }
-            else() {
-                printf("\n" "Import failed! You may either have invalid txt format or that you may have mistyped the txt file name. Please double-check your txt file and try again.");
+            else {
+                printf("\n" "Cannot find the txt file. Please double-check your file name and try again.");
             }
         }
-        
-        if(strcmp exit){ //if user types the exit command.
+        */
+        else if(strcmp(input, "exit") == 0){ //if user types the exit command.
             break;
-        };
+        }
 
-        else() { //if command is not recognised.
-            printf ("\n" "Invalid syntax. Type 'help' to show a list of syntaxes.")
-        };
+        else if(strcmp(input, "list") == 0){ //if user types the list command.
+            printf("\n" "Day 1: \n" "\n");
+            printf("%-35s %-13s %s\n" "%-35s %-13s %s\n", "Name", "Priority", "Duration", "----------------------------------", "------------", "----------");
+
+            i = 0;
+            numUsed = sizeof(schedule) / sizeof(schedule[0]);
+            if(i < numUsed){
+                printf("%-35s %-13d %s\n", schedule[i].name, schedule[i].priority, schedule[i].duration);
+                i = i+1;
+            }
+
+        }
+
+        else { //if command is not recognised.
+            printf ("\n" "Invalid syntax. Type 'help' to show a list of syntaxes.\n");
+        }
 
     };
     return 0;
-    */
-
-    struct schedules schedule1;
-    printf("Enter the name of the event... (30 characters max)\n");
-    scanf ("%s", schedule1.name);
-    printf("Enter the priority of the event... (1 for earliest)\n");
-    scanf ("%d", &schedule1.priority);
-    printf("Enter the duration of the event... (format HHMM)\n");
-    scanf ("%s", schedule1.duration);
-
-printf("\n" "Day 1: \n" "\n");
-printf("%-35s %-13s %s\n" "%-35s %-13s %s\n", "Name", "Priority", "Duration", "----------------------------------", "------------", "----------");
-printf("%-35s %-13d %s\n", schedule1.name, schedule1.priority, schedule1.duration);
-
-return 0;
 }
