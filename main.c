a75819dfd3714312b485bf7902d6df2eda592b51
Ni7r4m <georgediomartin@gmail.com>
2022-05-11 08:39:16 +0100

    Added note and a bit of loop for command-like terminal

--- main.c
+++ main.c
@@ -17,19 +17,62 @@ int main()
     printf("\n" "Starting Festival Scheduler... \n" "\n" "Type 'help' to show lists of syntaxes in the app.\n");
 
     /*
-    printf ("SCHEDULER>")
-    scanf ("%s", input);
+    while (1)
+    {
+        printf("\n" "SCHEDULER>")
+        scanf ("%s", input);
+        
+        if(insert strcmp help command here){  //if user types the help command.
+            printf("\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n",
+            "ADD", "Adds an event from the schedule.",
+            "DEL", "Removes an event from the schedule.",
+            "NEXT", "Shows the next day of the festival (up to day 7).",
+            "PREV", "Shows the previous day of the festival",
+            "IMPORT", "Imports events from a txt file.",
+            "EXIT", "Quits Festival Scheduler."); 
+        };
 
-    
-    if(insert strcmp help command here){ 
-        printf("\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n" "%-10s %s\n",
-        "ADD", "Adds an event from the schedule.",
-        "DEL", "Removes an event from the schedule.",
-        "NEXT", "Shows the next day of the festival (up to day 7).",
-        "PREV", "Shows the previous day of the festival",
-        "IMPORT", "Imports events from a txt file.",
-        "EXIT", "Quits Festival Scheduler."); 
-    }
+        if (strcmp del) { //if user types the del command.
+            printf("\n" "Please select an event to delete based on the priority number. \n");
+            scanf ("%s", input);
+
+            if(priority number is found on the schedule); {
+                printf("You are going to remove the selected event:\n" 
+                "\n" 
+                "Do you want to continue? (Y/N)\n")
+                scanf ("%s", input);
+
+                if (yes); {
+                    //insert remove command here
+                }
+            }
+            else() {
+                printf("Invalid priority number, please try again. \n");
+            }
+        }
+
+        if(strcmp import) { //if user types the import command.
+            printf();
+            scanf ("%s", input);
+
+            if(success) {
+                printf("\n" "Import successful!");
+            }
+            else() {
+                printf("\n" "Import failed! You may either have invalid txt format or that you may have mistyped the txt file name. Please double-check your txt file and try again.");
+            }
+        }
+        
+        if(strcmp exit){ //if user types the exit command.
+            break;
+        };
+
+        else() { //if command is not recognised.
+            printf ("\n" "Invalid syntax. Type 'help' to show a list of syntaxes.")
+        };
+
+    };
+    return 0;
     */
 
     struct schedules schedule1;
